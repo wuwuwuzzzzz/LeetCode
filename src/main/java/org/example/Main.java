2ac66509f14d8ca704af4eea5f2ca7245c9a4c5d
wxz <759126349@qq.com>
2022-12-01 18:04:59 +0800

    优化代码

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,5 +1,10 @@
 package org.example;
 
+/**
+ *
+ * @author wxz
+ * @date 17:58 2022/12/1
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
