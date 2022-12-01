ee70cb352467f49e134b9ab483bcbbb4805fed9e
wxz <759126349@qq.com>
2022-12-01 17:57:44 +0800

    优化代码

---
+++ src/leetcode/editor/cn/[剑指 Offer II 039]直方图最大矩形面积.java
@@ -0,0 +1,49 @@
+//给定非负整数数组 heights ，数组中的数字用来表示柱状图中各个柱子的高度。每个柱子彼此相邻，且宽度为 1 。 
+//
+// 求在该柱状图中，能够勾勒出来的矩形的最大面积。 
+//
+// 
+//
+// 示例 1: 
+//
+// 
+//
+// 
+//输入：heights = [2,1,5,6,2,3]
+//输出：10
+//解释：最大的矩形为图中红色区域，面积为 10
+// 
+//
+// 示例 2： 
+//
+// 
+//
+// 
+//输入： heights = [2,4]
+//输出： 4 
+//
+// 
+//
+// 提示： 
+//
+// 
+// 1 <= heights.length <=10⁵ 
+// 0 <= heights[i] <= 10⁴ 
+// 
+//
+// 
+//
+// 
+// 注意：本题与主站 84 题相同： https://leetcode-cn.com/problems/largest-rectangle-in-
+//histogram/ 
+//
+// Related Topics 栈 数组 单调栈 👍 67 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int largestRectangleArea(int[] heights) {
+
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
